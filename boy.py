2b983effffab93dc59c72ab73409221afb1bf398
chohwanhee(lggram) <great1625@gmail.com>
2023-10-19 11:51:11 +0900

    공 발사를 위한 사전 작업

--- boy.py
+++ boy.py
@@ -45,6 +45,8 @@ class Idle:
 
     @staticmethod
     def exit(boy, e):
+        if space_down(e):
+            boy.fire_ball()
         pass
 
     @staticmethod
@@ -70,7 +72,8 @@ class Run:
 
     @staticmethod
     def exit(boy, e):
-        pass
+        if space_down(e):
+            boy.fire_ball()
 
     @staticmethod
     def do(boy):
@@ -114,8 +117,8 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Idle
         self.transitions = {
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+            Idle: {space_down: Idle, right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
+            Run: {space_down: Run, right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
             Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle}
         }
 
@@ -161,3 +164,9 @@ class Boy:
 
     def draw(self):
         self.state_machine.draw()
+
+    def fire_ball(self):
+        if self.face_dir == 1:
+            print('fire_ball to right')
+        else:
+            print('fire_ball to left')
